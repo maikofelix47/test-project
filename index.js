60c64117d638fa1eac530c72f258b22972a9e8f0
SharleenAwinja <awinjasharleen@gmail.com>
2022-08-30 21:13:51 -0700

    Added division function

--- index.js
+++ index.js
@@ -20,9 +20,18 @@ const mySub = subtract(5, 3);
 
 console.log(mySub);
 
+
 function modulus(a, b) {
     return a%b;
 }
 
 const myMod = modulus(7, 3);
 console.log('Modulus:',myMod);
+
+function division(a, b) {
+    return a / b;
+}
+
+const myDiv = division(8, 4);
+console.log('Division:' ,myDiv);
+
