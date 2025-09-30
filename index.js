b69e7c210a70037288cce8594ee474d2618c9b4a
Felix Maiko <maikofelix89@gmail.com>
2025-09-30 19:39:46 +0300

    Add sum function (#1)

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ function displayWelcomeMessage() {
   console.log("Welcome....");
 }
 
+function add(a, b) {
+  return a + b;
+}
+
 displayWelcomeMessage();
+const mySum = add(3, 4);
+console.log({ mySum });
