b79b13198ecdb9934ac266e763b429de92a2df55
Your Name <youremail@example.com>
2025-10-07 19:33:18 +0300

    multiply two number

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function add(a, b) {
 function divide(a, b) {
   return a / b;
 }
+function multiply(a, b) {
+  return a * b;
+};
 
 displayWelcomeMessage();
 const mySum = add(3, 4);
-console.log({ mySum });
+
